20ef2491f3e42fe8ef8293a649b4349927dcfc9f
JulyKikuAkita <b92701105@gmail.com>
2022-04-11 23:02:00 -0700

    Day 92: How layout works in SwiftUI

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -4,13 +4,29 @@
 //
 //  Created by Ifang Lee on 4/11/22.
 //
-
+/*
+ -A parent view proposes a size for its child.
+ -Based on that information, the child then chooses its own size and the parent must respect that choice.
+ - The parent then positions the child in its coordinate space.
+ - Behind the scenes, SwiftUI performs a fourth step: although it stores positions and sizes as floating-point numbers, when it comes to rendering SwiftUI rounds off any pixels to their nearest values so our graphics remain sharp.
+ */
 import SwiftUI
 
+// applying modifiers creates new views rather than just modifying existing views in-place.
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group { // the text view becomes a child of its background
+            Text("Layout Neutral")
+                .background(.green) // layout neutral
+                .padding(20)
+
+            Text("Layout Neutral")
+                .padding(20)
+                .background(.green) //layout neutral
+
+            Color.blue
+        }
+
     }
 }
 
