3634cc4143e94e9cdcbc4bdbc6062b0f9bd62d12
JulyKikuAkita <b92701105@gmail.com>
2022-04-11 22:55:15 -0700

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Ifang Lee on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
