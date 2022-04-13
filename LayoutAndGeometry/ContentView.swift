baa0123fae37c6394ef55e73fb5ec50bcfb65493
JulyKikuAkita <b92701105@gmail.com>
2022-04-12 23:41:24 -0700

    day 92: Alignment and alignment guides

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -15,6 +15,88 @@ import SwiftUI
 // applying modifiers creates new views rather than just modifying existing views in-place.
 struct ContentView: View {
     var body: some View {
+        AlignmentView()
+        AlignmentGuideView()
+        simpleAlignmentView()
+        LayoutView()
+    }
+}
+
+struct AlignmentGuideView: View {
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                       Text("alignment: .leading")
+                       Text("This is a longer line of text")
+                   }
+                   .background(.red)
+                   .frame(width: 300, height: 300)
+                   .background(.blue)
+
+            // same as above
+            VStack(alignment: .leading) {
+                Text("alignment: .leading")
+                    .alignmentGuide(.leading) { d in d[.leading] }
+                Text("Rewrite in alignmentGuide")
+            }
+            .padding()
+
+            // alignment guide to use the view’s trailing edge for its leading alignment guide
+            VStack(alignment: .leading) {
+                Text("alignment: .leading")
+                    .alignmentGuide(.leading) { d in d[.trailing] }
+                Text("Rewrite in alignmentGuide")
+            }
+        }
+
+    }
+}
+
+struct AlignmentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.gray)
+    }
+}
+
+struct simpleAlignmentView: View {
+    var body: some View {
+        VStack {
+            Text("Live long and prosper")
+                .frame(width: 300, height: 300, alignment: .topLeading)
+
+            HStack(alignment: .bottom) {
+                Text("Live")
+                    .font(.caption)
+                Text("long")
+                Text("and")
+                    .font(.title)
+                Text("prosper")
+                    .font(.largeTitle)
+            }
+
+            HStack(alignment: .lastTextBaseline) { //try .bottom
+                Text("Live")
+                    .font(.caption)
+                Text("long")
+                Text("and")
+                    .font(.title)
+                Text("prosper")
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct LayoutView: View {
+    var body: some View {
         Group { // the text view becomes a child of its background
             Text("Layout Neutral")
                 .background(.green) // layout neutral
@@ -26,7 +108,6 @@ struct ContentView: View {
 
             Color.blue
         }
-
     }
 }
 
