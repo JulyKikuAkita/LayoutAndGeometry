c81e99dea451cbdfb2de8aaa1811851a2de7dfe0
JulyKikuAkita <b92701105@gmail.com>
2022-04-20 23:37:18 -0700

    day 94: challenge

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -25,6 +25,14 @@ struct RotationEffectBasicView: View {
     }
 }
 
+/*
+ Challenge:
+
+ Make views near the top of the scroll view fade out to 0 opacity – I would suggest starting at about 200 points from the top.
+ Make views adjust their scale depending on their vertical position, with views near the bottom being large and views near the top being small. I would suggest going no smaller than 50% of the regular size.
+ For a real challenge make the views change color as you scroll. For the best effect, you should create colors using the Color(hue:saturation:brightness:) initializer, feeding in varying values for the hue.
+
+ */
 struct NatualRotationView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
 
@@ -35,8 +43,10 @@ struct NatualRotationView: View {
                     GeometryReader { geo in
                         Text("Row #\(index)")
                             .font(.title)
-                            .frame(maxWidth: .infinity)
-                            .background(colors [index % 7])
+                            .frame(maxWidth: .infinity,
+                                   minHeight: geo.frame(in: .global).minY > fullView.size.height / 2 ? 5 : 50)
+                            .background(Color(hue: geo.frame(in: .local).minY, saturation: geo.frame(in: .local).maxX, brightness: geo.frame(in: .local).maxY))
+                            .opacity(geo.frame(in: .global).minY / 190)
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y :1, z: 0))
                     }
                     .frame(height: 40)
@@ -70,9 +80,9 @@ struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
 
     var body: some View {
-        RotationEffectBasicView()
+//        RotationEffectBasicView()
         NatualRotationView()
-        CoverFlowScrollView()
+//        CoverFlowScrollView()
     }
 }
 
