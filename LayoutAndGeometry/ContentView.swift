712cf60a3c98999024f50a41048a902b92d2fb73
JulyKikuAkita <b92701105@gmail.com>
2022-04-22 13:39:48 -0700

    Day 95: add challenge description

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+/*
+ https://www.hackingwithswift.com/guide/ios-swiftui/7/3/challenge
+ Day 95:
+ 1. Let the user customize the dice that are rolled: how many of them, and what type: 4-sided, 6-sided, 8-sided, 10-sided, 12-sided, 20-sided, and even 100-sided.
+ 2. Show the total rolled on the dice.
+ 3. Store the results using JSON or Core Data – anywhere they are persistent.
+ 4. Add haptic feedback when dice are rolled.
+ 5. For a real challenge, make the value rolled by the dice flick through various possible values before settling on the final figure.
+ When I say “roll dice” you don’t need to create fancy 3D effects – just showing the numbers that were “rolled” is fine.
+
+ The only thing that might cause you some work is step 5: making the results flick through various values before settling on the final figure. The easiest way to tackle this is through a Timer that gets cancelled after a certain number of calls.
+
+
+ */
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
